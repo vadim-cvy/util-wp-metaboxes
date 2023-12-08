83f4e1b36d112f799cbc268114e9a043162125b6
Vadim <vadim.cvo@gmail.com>
2023-12-08 12:02:12 +0200

    Make post screen check more flexible

--- src/PostMetabox.php
+++ src/PostMetabox.php
@@ -5,7 +5,7 @@ abstract class PostMetabox extends PostTypeMetabox
 {
   abstract protected function get_target_post_id() : int;
 
-  final protected function is_current_screen_authorized() : bool
+  protected function is_current_screen_authorized() : bool
   {
     return parent::is_current_screen_authorized()
       && $this->get_current_post_id() === $this->get_target_post_id();
